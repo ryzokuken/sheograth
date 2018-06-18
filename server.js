5062d97ba872c6af2e81d3bc7e28fa0c37d75073
Ujjwal Sharma <usharma1998@gmail.com>
2018-06-18 16:45:02 +0530

    Send something back

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require('express')();
 
-app.get('/', (req, res) => {console.log(req.headers.host.split('.')[0])};
+app.get('/', (req, res) => {
+  console.log(req.headers.host.split('.')[0]);
+  res.send(req.headers.host);
+});
 
 app.listen(80, () => console.log('Listening on port 80!'));
