630dc2e9590344955cd65d5f6e98e85bd6f139aa
Ujjwal Sharma <usharma1998@gmail.com>
2018-06-18 16:40:54 +0530

    Log host header

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('express')();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {console.log(req.headers.host.split('.')[0])};
 
 app.listen(80, () => console.log('Listening on port 80!'));
